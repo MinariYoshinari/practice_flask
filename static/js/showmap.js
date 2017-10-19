7082cad6d7bba017aa196c952407fdf11e768787
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-20 00:19:52 +0900

    add map to toppage

---
+++ static/js/showmap.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var opts = {
+      zoom: 15,
+      center: new google.maps.LatLng(38.255921,140.835757)
+    };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+}
