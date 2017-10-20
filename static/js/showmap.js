dbb9c81b76c5892576e433a90067731769164a20
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-20 15:24:42 +0900

    fix loading bootstrap in base.html

--- static/js/showmap.js
+++ static/js/showmap.js
@@ -1,9 +1,12 @@
-    function initMap() {
-      var opts = {
+function initMap() {
+    var center_lat = (origin_lat + destination_lat)/2;
+    var center_lng = (origin_lng + destination_lng)/2;
+
+    var opts = {
         zoom: 15,
-        center: new google.maps.LatLng(destination_lat,destination_lng)
-      };
-      var map = new google.maps.Map(document.getElementById("map"), opts);
-      console.log(origin_lat);
-      console.log(origin_lng);
-    }
+        center: new google.maps.LatLng(center_lat, center_lng)
+     };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+    console.log(origin_lat);
+    console.log(origin_lng);
+ }
